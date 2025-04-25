7bcf44017c70ff08831d226972700961b466d032
AutomotiveM <asmamulani0311@gmail.com>
2025-04-25 11:00:38 +0530

    Renaming function names as per the guidelines

--- src/BowlingGame.cpp
+++ src/BowlingGame.cpp
@@ -9,8 +9,8 @@ void BowlingGameNM::BowlingGame::run()
     std::cout << "\n" << PlayerObject.playerName() << " starts game...\n";
     PlayerObject.getThrowScore();
     if (PlayerObject.calculateFrameScore() == MAX_SCORE)
-       {
-         std::cout << "\n Got maximum score!\n";
-       }
-         std::cout << "\n" << PlayerObject.playerName() << " Game Finished...\n";
+    {
+        std::cout << "\n Got maximum score!\n";
+    }
+    std::cout << "\n" << PlayerObject.playerName() << " Game Finished...\n";
 }
